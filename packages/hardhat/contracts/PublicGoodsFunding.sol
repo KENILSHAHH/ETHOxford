a7f8e59b37e5597e363faee1b8d233f0a1ca5c67
kenilshahh <kenilshah1505@gmai.com>
2024-03-09 20:52:47 +0530

    added public goods funding contract

---
+++ packages/hardhat/contracts/PublicGoodsFunding.sol
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import { Project } from "./Project.sol";
+import { Monkey } from "./Monkey.sol";
+import  "@openzeppelin/contracts/access/Ownable.sol";
+
+contract PublicGoodsFunding is Ownable(msg.sender) {
+	// State variables
+	Monkey nftContract;
+	mapping(address => bool) projects;
+
+	//Events
+	event ProjectCreated(
+		address indexed projectAddress,
+		address indexed projectOwner
+	);
+	event NFTMinted(address projectAddress, address contributor);
+
+	// Functions
+	constructor(Monkey _nftContract) {
+		nftContract = _nftContract;
+	}
+
+	/**
+	 * Create public goods project campaign
+	 */
+	function createProject(
+		string memory projectTitle,
+		string memory projectDescription,
+		address projectTokenAddress, // this shouldnt be a parameter. we should eventually control which addresses can be assigned to
+		uint256 targetAmount,
+		uint256 deadline,
+		string memory image
+	) public {
+		require(
+			deadline > block.timestamp,
+			"Deadline must be set in the future"
+		);
+
+		Project project = new Project(
+			address(this),
+			msg.sender,
+			projectTokenAddress,
+			projectTitle,
+			projectDescription,
+			targetAmount,
+			deadline,
+			image
+		);
+		projects[address(project)] = true;
+		emit ProjectCreated(address(project), msg.sender);
+	}
+
+	function mintToFunder() public {
+		require(
+			projects[msg.sender],
+			"Only campaign project contracts can mint NFTs"
+		);
+		if (nftContract.balanceOf(tx.origin) == 0) {
+			nftContract.mintNft(tx.origin);
+			emit NFTMinted(msg.sender, tx.origin);
+		}
+	}
+
+	function setNewBaseUri(string memory _baseUri) public onlyOwner {
+		nftContract.setBaseUri(_baseUri);
+	}
+
+	function setMaxSupply(uint256 _maxSupply) public onlyOwner {
+		nftContract.setMaxSupply(_maxSupply);
+	}
+}
